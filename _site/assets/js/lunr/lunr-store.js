fac28c2735b58390dcf901a8071d551f84fa4556
LEE SUNGJIN <ssjj3552@khu.ac.kr>
2024-05-27 16:52:24 +0900

    v0.4.1

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,38 +5,44 @@ var store = [{
         "url": "/lab_meeting/1/",
         "teaser": null
       },{
-        "title": "",
-        "excerpt":"   발제자: 최호성     발제일: 2024년 05월 17일     키워드:  [Cell on/off]  [Energy-saving]  [Network]  [Artificial Intelligence]     URL:      발표자료:    제목 1   ","categories": [],
+        "title": "Open RAN for detection of a jamming attack in a 5G network",
+        "excerpt":"발제자: 최호성 발제일: 2024년 05월 17일 키워드: [OpenRAN] [jamming detection] [Kolmogorov-Smirnov Test] URL: https://ieeexplore.ieee.org/document/10201067 발표자료: PDF Why this paper 무선 네트워크에 대한 jamming을 O-RAN RIC의 siangl quailty indicators를 통해 해결 네트워크 구조나 UE 분포에 대한 가정을 사용하지 않은 가벼운 algorithm 사용 Summary of paper 1. Introduction 1.1 Jamming 주파수대역에 신호를...","categories": [],
         "tags": [],
         "url": "/lab_meeting/2/",
         "teaser": null
       },{
-        "title": "Test 1",
-        "excerpt":"Test 1  ","categories": ["daily"],
+        "title": "LG CNS, 경희대에 '이음5G' 구축…5G특화망 사업 본격화",
+        "excerpt":"LG CNS가 공장, 건물 등 특정 범위에 한해 5G 통신망을 구현할 수 있는 ‘이음5G’ 사업을 본격화한다, LG CNS는 경희대와 ‘이음5G 구축 및 연구·개발’을 위한 업무협약을 체결했다고 16일 밝혔다. 양측은 이음5G를 기반으로 제조·물류 등 산업현장에 적용할 수 있는 솔루션과 서비스 연구·개발을 확대해 나갈 계획이다. LG CNS와 경희대는 이번 협약으로 이음5G 기반의...","categories": ["news"],
         "tags": [],
-        "url": "/news/2024-02-10-test-1/",
+        "url": "/news/3/",
         "teaser": null
       },{
-        "title": "Test 2",
-        "excerpt":"Test 2  ","categories": ["daily"],
+        "title": "\"오픈랜, 지능·속도 다 잡은 6G 시대 필수기술\"",
+        "excerpt":"“6G의 가장 큰 특징은 인공지능(AI)이다. 네트워크에 AI 기술을 적용해 5G 보다 성능을 올릴 것으로 기대된다. 이 때문에 6G에서는 개방화, 지능화가 특징인 오픈랜 기술 필요성이 높아진다.” 한국통신학회장을 맡고 있는 홍인기 경희대 전자공학과 교수는 오픈랜 활성화의 중요성에 대해 이와 같이 말했다. 홍 교수는 5G 확산과 차세대 이동통신 기술 발전을 위해 오픈랜 기술을...","categories": ["news"],
         "tags": [],
-        "url": "/news/2024-02-10-test-2-copy/",
+        "url": "/news/2/",
+        "teaser": null
+      },{
+        "title": "경희대 이공계 홍보영상",
+        "excerpt":" ","categories": ["news"],
+        "tags": [],
+        "url": "/news/1/",
         "teaser": null
       },,{
-    "title": "[IEEE ‘20] Cell On/Off Parameter Optimization for Saving Energy via Reinforcement Learning",
-    "excerpt":"발제자: 1.md 발제일: 2024년 05월 10일 키워드: [Cell on/off] [Energy-saving] [Network] [Artificial Intelligence] URL: https://ieeexplore.ieee.org/document/9682160 발표자료: PDF Why this paper Energy-saving을 실현하고 QoS 기준을 만족하는 cell 온오프 알고리즘을 RL 문제로 정의 다양한 Operational mode에서 training된 RL agent들간의 성능을 비교 현실 세계를 모방한 replicatvie simulator를 활용하여 training한 모델 사용 Summary of...","url": "http://localhost:4000/_lab_meeting/1/"
-  },{
     "title": "Git",
     "excerpt":"   ","url": "http://localhost:4000/study/programming/github/1/"
   },{
-    "title": "딥 러닝",
-    "excerpt":"딥 러닝은 인간 두뇌의 구조를 본 따 개발된 인공지능이다. 인간이 사물을 인식하기의 과정은, 먼저 눈으로 보고 그 정보가 여러 뇌를 거치게 되는데 마찬가지로 딥 러닝 또한 여러 계층으로 구성된다. 인간의 신경과 뇌에는 수백만개의 뉴런이 연결되어 있듯 딥 러닝 신경망도 인공 신경 세포 노드를 통해 계층을 구성하고 있다. 딥 러닝의 목표는...","url": "http://localhost:4000/study/ai/2/"
+    "title": "[IEEE ‘20] Cell On/Off Parameter Optimization for Saving Energy via Reinforcement Learning",
+    "excerpt":"발제자: 1.md 발제일: 2024년 05월 10일 키워드: [Cell on/off] [Energy-saving] [Network] [Artificial Intelligence] URL: https://ieeexplore.ieee.org/document/9682160 발표자료: PDF Why this paper Energy-saving을 실현하고 QoS 기준을 만족하는 cell 온오프 알고리즘을 RL 문제로 정의 다양한 Operational mode에서 training된 RL agent들간의 성능을 비교 현실 세계를 모방한 replicatvie simulator를 활용하여 training한 모델 사용 Summary of...","url": "http://localhost:4000/_lab_meeting/1/"
   },{
-    "title": "",
-    "excerpt":"   발제자: 2.md     발제일: 2024년 05월 17일     키워드:  [Cell on/off]  [Energy-saving]  [Network]  [Artificial Intelligence]     URL:      발표자료:    제목 1   ","url": "http://localhost:4000/_lab_meeting/2/"
+    "title": "Open RAN for detection of a jamming attack in a 5G network",
+    "excerpt":"발제자: 2.md 발제일: 2024년 05월 17일 키워드: [OpenRAN] [jamming detection] [Kolmogorov-Smirnov Test] URL: https://ieeexplore.ieee.org/document/10201067 발표자료: PDF Why this paper 무선 네트워크에 대한 jamming을 O-RAN RIC의 siangl quailty indicators를 통해 해결 네트워크 구조나 UE 분포에 대한 가정을 사용하지 않은 가벼운 algorithm 사용 Summary of paper 1. Introduction 1.1 Jamming 주파수대역에 신호를...","url": "http://localhost:4000/_lab_meeting/2/"
   },{
     "title": "Github 개요",
     "excerpt":"git init git add README.md git commit -e git branch -M main git remote add origin https://github.com/&lt;user_name&gt;/&lt;repository_name&gt;.git git push -u origin main      ","url": "http://localhost:4000/study/programming/github/2/"
+  },{
+    "title": "딥 러닝",
+    "excerpt":"딥 러닝은 인간 두뇌의 구조를 본 따 개발된 인공지능이다. 인간이 사물을 인식하기의 과정은, 먼저 눈으로 보고 그 정보가 여러 뇌를 거치게 되는데 마찬가지로 딥 러닝 또한 여러 계층으로 구성된다. 인간의 신경과 뇌에는 수백만개의 뉴런이 연결되어 있듯 딥 러닝 신경망도 인공 신경 세포 노드를 통해 계층을 구성하고 있다. 딥 러닝의 목표는...","url": "http://localhost:4000/study/ai/2/"
   },{
     "title": "Page Not Found",
     "excerpt":"         404   Page Not Found                               ","url": "http://localhost:4000/404.html"
@@ -63,7 +69,7 @@ var store = [{
     "excerpt":"","url": "http://localhost:4000/"
   },{
     "title": "Lab Meeting",
-    "excerpt":"                                                                                                                                                                                                              May 17, 2024                      최호성                                                                                                                                                                                          [IEEE ‘20] Cell On/Off Parameter Optimization for Saving Energy via Reinforcement Learning                                                       May 10, 2024                      김건                                                     ","url": "http://localhost:4000/lab_meeting/"
+    "excerpt":"                                                                                                                                                       Open RAN for detection of a jamming attack in a 5G network                                                       May 17, 2024                      최호성                                                                                                                                                                                          [IEEE ‘20] Cell On/Off Parameter Optimization for Saving Energy via Reinforcement Learning                                                       May 10, 2024                      김건                                                     ","url": "http://localhost:4000/lab_meeting/"
   },{
     "title": null,
     "excerpt":"var idx = lunr(function () { this.field('title') this.field('excerpt') this.field('categories') this.field('tags') this.ref('id') this.pipeline.remove(lunr.trimmer) for (var item in store) { this.add({ title: store[item].title, excerpt: store[item].excerpt, categories: store[item].categories, tags: store[item].tags, id: item }) } }); $(document).ready(function() { $('input#search').on('keyup', function () { var resultdiv = $('#results'); var query = $(this).val().toLowerCase(); var result = idx.query(function...","url": "http://localhost:4000/assets/js/lunr/lunr-en.js"
@@ -76,6 +82,9 @@ var store = [{
   },{
     "title": "Building Flexible, Low-Cost Wireless Access Networks With Magma",
     "excerpt":"","url": "http://localhost:4000/study/paper_review/magma_001/"
+  },{
+    "title": "Members",
+    "excerpt":"                          Members                                        Professor                                     Students                        ","url": "http://localhost:4000/members/"
   },{
     "title": "News",
     "excerpt":"","url": "http://localhost:4000/news/"
